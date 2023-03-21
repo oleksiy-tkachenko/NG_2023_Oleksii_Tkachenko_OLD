4a199b234d11ae8e045aff058e9333688814f0ba
Sans91 <sansomailjokes@gmail.com>
2023-03-21 11:13:51 +0200

    Modified L_1/Task_2. Added the ending to phrase

--- Lection_1/Task_2/main.py
+++ Lection_1/Task_2/main.py
@@ -1,4 +1,4 @@
 name = input("Write your Name: ")
 surname = input("Write your Surname: ")
 print("You know, for the most wanted man in the sector, you ain't that hard to find."
-    f"\nI had to see it for myself... Little {name} {surname}")
+    f"\nI had to see it for myself... Little {name} {surname}... the people's hero.")
