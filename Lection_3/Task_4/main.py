1c004515519411eec83c2067eae0b22b1c219297
Sans91 <sansomailjokes@gmail.com>
2023-04-10 19:08:30 +0300

    Patched L_3/Task_4. Made try...except structure to prevent crashes from invalid links

--- Lection_3/Task_4/main.py
+++ Lection_3/Task_4/main.py
@@ -1,8 +1,12 @@
 import requests
 linkList = input("Input your links with spaces between them:\n").split()
 for link in linkList:
-    linkStatusCode = requests.request("GET", link).status_code
-    if (linkStatusCode==200 or linkStatusCode==302):
-        print("OK")
+    try:
+        linkStatusCode = requests.request("GET", link).status_code
+    except:
+        print("unknown error - FAIL")
     else:
-        print("FAIL")
+        if (linkStatusCode==200 or linkStatusCode==302):
+            print(f"{linkStatusCode} - OK")
+        else:
+            print(f"{linkStatusCode} - FAIL")
