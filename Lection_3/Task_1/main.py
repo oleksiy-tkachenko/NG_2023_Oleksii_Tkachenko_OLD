9bbba0c049dda51f4a34311ad6b00df865ecef86
Sans91 <sansomailjokes@gmail.com>
2023-04-20 20:55:32 +0300

    Edited L_3/Task_1. Made it full of functions!

--- Lection_3/Task_1/main.py
+++ Lection_3/Task_1/main.py
@@ -1,26 +1,39 @@
+def printFinalResult():
+    finalResult =  choiceCalculs(float(input("Write number A: ")), float(input("Write number B: ")))
+    if finalResult != None:
+        print(round(finalResult, 2))
+
 def division(numberA, numberB):
     if numberB == 0:
         print("infinity")
     else:
         return numberA/numberB
 
+def addition(numberA, numberB):
+    return numberA+numberB
+
+def subtraction(numberA, numberB):
+    return numberA-numberB
+
+def multiplication(numberA, numberB):
+    return numberA*numberB
+
+
 def choiceCalculs(numberA, numberB):
     calcResult = None
     match input("Available operations: +, -, *, /, power, root. Choose operation: "):
         case '+':
-            calcResult =numberA+numberB
+            calcResult = addition(numberA, numberB)
         case '-':
-            calcResult =numberA-numberB
+            calcResult = subtraction(numberA, numberB)
         case '*':
-            calcResult =numberA*numberB
+            calcResult = multiplication(numberA, numberB)
         case '/':
             calcResult = division(numberA, numberB)
         case 'power':
-            calcResult =numberA**numberB
+            calcResult = pow(numberA,numberB)
         case 'root':
             calcResult = pow(numberA, 1/numberB)
     return calcResult
 
-finalResult =  choiceCalculs(float(input("Write number A: ")), float(input("Write number B: ")))
-if finalResult != None:
-    print(round(finalResult, 2))
+printFinalResult()
